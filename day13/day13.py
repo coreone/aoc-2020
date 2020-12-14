9def65036ef511b053824d27bb45b6bb727443fa
Andrew Teixeira <teixeira@broadinstitute.org>
2020-12-13 20:16:52 -0500

    bah

--- day13/day13.py
+++ day13/day13.py
@@ -6,13 +6,14 @@ import sys
 
 def get_input(file):
     """Get formatted input from the file."""
-    ret = {"timestamp": 0, "buses": {}}
+    ret = {"timestamp": 0, "buses": {}, "min": 0, "max": 0}
     count = 0
     with open(file) as filep:
         ret["timestamp"] = int(filep.readline().rstrip())
         notes = filep.readline().rstrip()
         pieces = notes.split(",")
         bus = None
+        ret["min"] = int(pieces[0])
         for val in pieces:
             if val.isnumeric():
                 bus = int(val)
@@ -20,6 +21,10 @@ def get_input(file):
                     "count": count,
                     "x": [],
                 }
+                if bus < ret["min"]:
+                    ret["min"] = bus
+                if bus > ret["max"]:
+                    ret["max"] = bus
             else:
                 ret["buses"][bus]["x"].append(val)
             count += 1
@@ -45,13 +50,12 @@ def solution1(schedule_data):
 def solution2_infinity(schedule_data):
     """Find solution 2."""
     first_bus = list(schedule_data["buses"].keys())[0]
-    first_timestamp = schedule_data["timestamp"]
-    while True:
-        if first_timestamp % first_bus == 0:
-            break
-        first_timestamp += 1
-
-    timestamp = first_timestamp
+    timestamp = first_bus
+    # while True:
+    #     if timestamp % first_bus == 0:
+    #         break
+    #     timestamp += 1
+    print(timestamp)
     num_buses = len(schedule_data["buses"])
     found = 0
     while found < num_buses:
@@ -63,14 +67,20 @@ def solution2_infinity(schedule_data):
         if found < num_buses:
             found = 0
             timestamp += first_bus
+            # timestamp += first_bus * 173900
+            # timestamp += first_bus * schedule_data["min"]
+            # timestamp += first_bus * 3
 
     return timestamp
 
 
 def main():
     """Execute the script."""
-    schedule_data = get_input("input.txt")
+    # schedule_data = get_input("input.txt")
     schedule_data = get_input("test7.txt")
+    for s in schedule_data["buses"]:
+        print(f"{s}: {schedule_data['buses'][s]}")
+    # return 0
     product = solution1(schedule_data)
     print(f"Problem 1: bus product: {product}")
     timestamp = solution2_infinity(schedule_data)
